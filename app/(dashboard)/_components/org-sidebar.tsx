0b23a5ac31f27a7bf98fbdd6c1703cabf9114b6c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-02-10 17:07:57 +0000

    org sidebar layout created

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const OrgSidebar = () => {
+  return (
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
+      Org Sidebar
+    </div>
+  );
+};
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import type { PropsWithChildren } from "react";
 
+import { OrgSidebar } from "./_components/org-sidebar";
 import { Sidebar } from "./_components/sidebar";
 
 const DashboardLayout = ({ children }: PropsWithChildren) => {
@@ -7,7 +8,16 @@ const DashboardLayout = ({ children }: PropsWithChildren) => {
     <main className="h-full">
       <Sidebar />
 
-      <div className="pl-[60px] h-full">{children}</div>
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">
+            {/* Navbar */}
+
+            {children}
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
