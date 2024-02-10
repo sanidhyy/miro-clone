11031745d60eff785b053fb27f9ea8f5d520d6b1
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-02-10 16:37:09 +0000

    env types added

---
+++ environment.d.ts
@@ -0,0 +1,11 @@
+// This file is needed to support autocomplete for process.env
+export {};
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      // convex public url
+      NEXT_PUBLIC_CONVEX_URL: string;
+    }
+  }
+}
