1d38b0cd91afc26ba7a59330315cf18b4dd92029
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-02-10 16:33:44 +0000

    convex auth config created

---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://amusing-quetzal-49.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
