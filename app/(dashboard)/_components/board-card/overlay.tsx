b044e0be86901cbc625b8201597236c430cc5ea2
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-02-11 17:27:50 +0000

    board list implemented

---
+++ app/(dashboard)/_components/board-card/overlay.tsx
@@ -0,0 +1,8 @@
+export const Overlay = () => {
+  return (
+    <div
+      className="opacity-0 group-hover:opacity-50 transition-opacity h-full w-full bg-black"
+      aria-hidden
+    />
+  );
+};
--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import { useQuery } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+
+import { BoardCard } from "./board-card";
 import { EmptyBoards } from "./empty-boards";
 import { EmptyFavourites } from "./empty-favourites";
 import { EmptySearch } from "./empty-search";
@@ -13,7 +18,9 @@ type BoardListProps = {
 };
 
 export const BoardList = ({ orgId, query }: BoardListProps) => {
-  const data = []; // TODO: Change to API Call
+  const data = useQuery(api.boards.get, { orgId });
+
+  if (data === undefined) return <div>Loading...</div>;
 
   if (!data?.length && query.search) {
     return <EmptySearch />;
@@ -27,5 +34,27 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
     return <EmptyBoards />;
   }
 
-  return <div className="">BoardList</div>;
+  return (
+    <div className="">
+      <h2 className="text-3xl">
+        {query.favourites ? "Favourite boards" : "Team boards"}
+      </h2>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        {data?.map((board) => (
+          <BoardCard
+            key={board._id}
+            id={board._id}
+            title={board.title}
+            imageUrl={board.imageUrl}
+            authorId={board.authorId}
+            authorName={board.authorName}
+            createdAt={board._creationTime}
+            orgId={board.orgId}
+            isFavourite={false}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
