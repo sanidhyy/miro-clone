47e4c1f02b4024b3557f8ce3e706f8c2da069558
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-02-11 14:21:42 +0000

    navbar completed

---
+++ app/(dashboard)/_components/invite-button.tsx
@@ -0,0 +1,22 @@
+import { Plus } from "lucide-react";
+import { OrganizationProfile } from "@clerk/nextjs";
+
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+export const InviteButton = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          <Plus className="h-4 w-4 mr-2" />
+          Invite members
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent className="p-0 bg-transparent border-none max-w-[880px]">
+        <OrganizationProfile />
+      </DialogContent>
+    </Dialog>
+  );
+};
--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,16 +1,49 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import {
+  OrganizationSwitcher,
+  UserButton,
+  useOrganization,
+} from "@clerk/nextjs";
 
+import { InviteButton } from "./invite-button";
 import { SearchInput } from "./search-input";
 
 export const Navbar = () => {
+  const { organization } = useOrganization();
+
   return (
     <div className="flex items-center gap-x-4 p-5">
       <div className="hidden lg:flex lg:flex-1">
         <SearchInput />
       </div>
 
+      <div className="block lg:hidden flex-1">
+        <OrganizationSwitcher
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                maxWidth: "376px",
+              },
+              organizationSwitcherTrigger: {
+                padding: "6px",
+                width: "100%",
+                borderRadius: "8px",
+                border: "1px solid #E5E7EB",
+                justifyContent: "space-between",
+                backgroundColor: "white",
+              },
+            },
+          }}
+        />
+      </div>
+
+      {organization && <InviteButton />}
       <UserButton />
     </div>
   );
