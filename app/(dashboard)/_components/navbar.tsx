b9cec1b600a21fc4f8ec592fea904c92495cef92
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-02-11 14:14:26 +0000

    search input completed

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -2,10 +2,14 @@
 
 import { UserButton } from "@clerk/nextjs";
 
+import { SearchInput } from "./search-input";
+
 export const Navbar = () => {
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
-      <div className="hidden lg:flex lg:flex-1 bg-yellow-500">Search</div>
+    <div className="flex items-center gap-x-4 p-5">
+      <div className="hidden lg:flex lg:flex-1">
+        <SearchInput />
+      </div>
 
       <UserButton />
     </div>
---
+++ app/(dashboard)/_components/search-input.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Search } from "lucide-react";
+import { useRouter } from "next/navigation";
+import qs from "query-string";
+import { type ChangeEvent, useEffect, useState } from "react";
+import { useDebounceValue } from "usehooks-ts";
+
+import { Input } from "@/components/ui/input";
+
+export const SearchInput = () => {
+  const router = useRouter();
+  const [value, setValue] = useState("");
+  const [debouncedValue] = useDebounceValue(value, 500);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: {
+          search: debouncedValue,
+        },
+      },
+      { skipEmptyString: true, skipNull: true }
+    );
+
+    router.push(url);
+  }, [debouncedValue, router]);
+
+  return (
+    <div className="w-full relative">
+      <Search className="absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground h-4 w-4" />
+      <Input
+        className="w-full max-w-[516px] pl-9"
+        placeholder="Search boards"
+        onChange={handleChange}
+        value={value}
+      />
+    </div>
+  );
+};
