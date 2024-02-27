8a5e537ef17165ec8eab9ab1efef225776a1f6fe
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-02-27 16:10:43 +0000

    rename modal store hook created

---
+++ store/use-rename-modal.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+
+const defaultValues = { id: "", title: "" };
+
+type IRenameModal = {
+  isOpen: boolean;
+  initialValues: typeof defaultValues;
+  onOpen: (id: string, title: string) => void;
+  onClose: () => void;
+};
+
+export const useRenameModal = create<IRenameModal>((set) => ({
+  isOpen: false,
+  onOpen: (id, title) =>
+    set({
+      isOpen: true,
+      initialValues: { id, title },
+    }),
+  onClose: () =>
+    set({
+      isOpen: false,
+      initialValues: defaultValues,
+    }),
+  initialValues: defaultValues,
+}));
