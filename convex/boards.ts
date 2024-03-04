5b5488365e70dea5682bef3f1ac977137bd73417
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-04 12:35:14 +0000

    boards search query by title implemented

--- convex/boards.ts
+++ convex/boards.ts
@@ -5,17 +5,30 @@ import { query } from "./_generated/server";
 export const get = query({
   args: {
     orgId: v.string(),
+    search: v.optional(v.string()),
   },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
 
     if (!identity) throw new Error("Unauthorized.");
 
-    const boards = await ctx.db
-      .query("boards")
-      .withIndex("by_org", (q) => q.eq("orgId", args.orgId))
-      .order("desc")
-      .collect();
+    const title = args.search as string;
+    let boards = [];
+
+    if (title) {
+      boards = await ctx.db
+        .query("boards")
+        .withSearchIndex("search_title", (q) =>
+          q.search("title", title).eq("orgId", args.orgId)
+        )
+        .collect();
+    } else {
+      boards = await ctx.db
+        .query("boards")
+        .withIndex("by_org", (q) => q.eq("orgId", args.orgId))
+        .order("desc")
+        .collect();
+    }
 
     const boardsWithFavouriteRelation = boards.map((board) => {
       return ctx.db
