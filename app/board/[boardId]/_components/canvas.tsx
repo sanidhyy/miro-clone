4060e2d38fbf69d385b807a91d96f30857bb57a6
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-04 14:58:16 +0000

    info component implemented

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -4,20 +4,15 @@ import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
 
-import { useSelf } from "@/liveblocks.config";
-
 type CanvasProps = {
   boardId: string;
 };
 
 export const Canvas = ({ boardId }: CanvasProps) => {
-  const info = useSelf((me) => me.info);
-
-  console.log(info);
 
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
-      <Info />
+      <Info boardId={boardId} />
       <Participants />
       <Toolbar />
     </main>
--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,9 +1,84 @@
 "use client";
 
-export const Info = () => {
+import { useQuery } from "convex/react";
+import { Menu } from "lucide-react";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+
+import { Actions } from "@/components/actions";
+import { Hint } from "@/components/hint";
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import type { Id } from "@/convex/_generated/dataModel";
+import { cn } from "@/lib/utils";
+import { useRenameModal } from "@/store/use-rename-modal";
+
+const TabSeparator = () => <div className="text-neutral-300 px-1.5">|</div>;
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+type InfoProps = {
+  boardId: string;
+};
+
+export const Info = ({ boardId }: InfoProps) => {
+  const { onOpen } = useRenameModal();
+  const data = useQuery(api.board.get, {
+    id: boardId as Id<"boards">,
+  });
+
+  if (!data) return <InfoSkeleton />;
+
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-      TODO: Information about the board.
+      <Hint label="Go to boards" side="bottom" sideOffset={10}>
+        <Button variant="board" className="px-2" asChild>
+          <Link href="/">
+            <Image
+              src="/logo.svg"
+              alt="Miro Clone Logo"
+              height={40}
+              width={40}
+            />
+            <span
+              className={cn(
+                "font-semibold text-xl ml-2 text-black",
+                font.className
+              )}
+            >
+              Miro
+            </span>
+          </Link>
+        </Button>
+      </Hint>
+
+      <TabSeparator />
+
+      <Hint label="Edit title" side="bottom" sideOffset={10}>
+        <Button
+          onClick={() => onOpen(data._id, data.title)}
+          variant="board"
+          className="text-base font-normal px-2"
+        >
+          {data.title}
+        </Button>
+      </Hint>
+
+      <TabSeparator />
+
+      <Actions id={data._id} title={data.title} side="bottom" sideOffset={10}>
+        <div className="">
+          <Hint label="Main menu" side="bottom" sideOffset={10}>
+            <Button size="icon" variant="board">
+              <Menu />
+            </Button>
+          </Hint>
+        </div>
+      </Actions>
     </div>
   );
 };
