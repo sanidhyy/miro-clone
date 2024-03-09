47d5ba1581ccecdb1d0445beac77fe429fa32f1f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-09 13:47:24 +0000

    fix corner resize layer util

--- lib/utils.ts
+++ lib/utils.ts
@@ -38,22 +38,22 @@ export function resizeBounds(bounds: XYWH, corner: Side, point: Point): XYWH {
     height: bounds.height,
   };
 
-  if ((corner && Side.Left) === Side.Left) {
+  if ((corner & Side.Left) === Side.Left) {
     result.x = Math.min(point.x, bounds.x + bounds.width);
     result.width = Math.abs(bounds.x + bounds.width - point.x);
   }
 
-  if ((corner && Side.Right) === Side.Right) {
+  if ((corner & Side.Right) === Side.Right) {
     result.x = Math.min(point.x, bounds.x);
     result.width = Math.abs(point.x - bounds.x);
   }
 
-  if ((corner && Side.Top) === Side.Top) {
+  if ((corner & Side.Top) === Side.Top) {
     result.y = Math.min(point.y, bounds.y + bounds.height);
     result.height = Math.abs(bounds.y + bounds.height - point.y);
   }
 
-  if ((corner && Side.Bottom) === Side.Bottom) {
+  if ((corner & Side.Bottom) === Side.Bottom) {
     result.y = Math.min(point.y, bounds.y);
     result.height = Math.abs(point.y - bounds.y);
   }
