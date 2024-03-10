ddda21903109fe029dc177f84d12e6491755616a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-10 09:50:21 +0000

    site config added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import { Suspense } from "react";
 
 import { Loading } from "@/components/auth/loading";
 import { Toaster } from "@/components/ui/sonner";
+import { siteConfig } from "@/config";
 import { ConvexClientProvider } from "@/providers/convex-client-provider";
 import { ModalProvider } from "@/providers/modal-provider";
 
@@ -11,9 +12,10 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Miro Clone",
-  description: "Real-Time Next.js 14 Miro Clone.",
+export const metadata: Metadata = siteConfig;
+
+export const viewport: Viewport = {
+  themeColor: "#fff",
 };
 
 export default function RootLayout({
---
+++ config/index.ts
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "Miro Clone",
+  description:
+    "Revolutionary Miro clone: Real-time collaboration, versatile canvas, rich media, secure.",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "vercel",
+    "react",
+    "miro",
+    "board",
+    "shadcn",
+    "shadcn-ui",
+    "radix-ui",
+    "cn",
+    "clsx",
+    "draw",
+    "convex",
+    "sonner",
+    "liveblocks",
+    "zod",
+    "nosql",
+    "lucide-react",
+    "next-themes",
+    "clerk-themes",
+    "clerk",
+    "postcss",
+    "prettier",
+    "react-dom",
+    "tailwindcss",
+    "tailwindcss-animate",
+    "ui/ux",
+    "js",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "Sanidhya Kumar Verma",
+    url: "https://github.com/sanidhyy",
+  },
+} as const;
+
+export const links = {
+  sourceCode: "https://github.com/sanidhyy/miro-clone",
+} as const;
