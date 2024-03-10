e830d5306f7a4650180e0ce57c5c442fa342c5f4
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-10 08:09:59 +0000

    types added for pencil layer

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -59,6 +59,8 @@ const client = createClient({
 type Presence = {
   cursor: { x: number; y: number } | null;
   selection: string[];
+  pencilDraft: [x: number, y: number, pressure: number][] | null;
+  penColor: Color | null;
 };
 
 // Optionally, Storage represents the shared document that persists in the
