eed1b96b1fd7d80701274e22dfa74c8fc987af8b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-10 08:25:43 +0000

    pen points to path layer util function created

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,6 +9,8 @@ import {
   Side,
   type Point,
   type Layer,
+  type PathLayer,
+  LayerType,
 } from "@/types/canvas";
 
 const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
@@ -106,4 +108,36 @@ export function getContrastingTextColor(color: Color) {
   const luminance = 0.299 * color.r + 0.587 * color.g + 0.114 * color.b;
 
   return luminance > 182 ? "black" : "white";
+}
+
+export function penPointsToPathLayer(
+  points: number[][],
+  color: Color
+): PathLayer {
+  if (points.length < 2)
+    throw new Error("Cannot transform points with less than 2 points.");
+
+  let left = Number.POSITIVE_INFINITY;
+  let top = Number.POSITIVE_INFINITY;
+  let right = Number.NEGATIVE_INFINITY;
+  let bottom = Number.NEGATIVE_INFINITY;
+
+  for (const point of points) {
+    const [x, y] = point;
+
+    if (left > x) left = x;
+    if (top > y) top = y;
+    if (right < x) right = x;
+    if (bottom < y) bottom = y;
+  }
+
+  return {
+    type: LayerType.Path,
+    x: left,
+    y: top,
+    width: right - left,
+    height: bottom - top,
+    fill: color,
+    points: points.map(([x, y, pressure]) => [x - left, y - top, pressure]),
+  };
 }
