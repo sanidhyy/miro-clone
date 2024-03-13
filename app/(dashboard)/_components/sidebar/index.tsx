66ea5ee1d3679e753c4eb651643c043a4bcca889
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-13 06:13:23 +0000

    source code link added

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,11 +1,31 @@
+import { Github } from "lucide-react";
+
+import { Hint } from "@/components/hint";
+import { links } from "@/config";
+
 import { List } from "./list";
 import { NewButton } from "./new-button";
 
 export const Sidebar = () => {
   return (
-    <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white">
-      <List />
-      <NewButton />
+    <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col justify-between text-white">
+      <div className="flex flex-col gap-y-4">
+        <List />
+        <NewButton />
+      </div>
+
+      <div className="aspect-square">
+        <Hint label="Source Code" side="right" align="start" sideOffset={18}>
+          <a
+            href={links.sourceCode}
+            target="_blank"
+            rel="noreferrer noopener"
+            className="bg-white/25 h-full w-full flex items-center justify-center rounded-md opacity-60 hover:opacity-100 transition"
+          >
+            <Github className="text-white h-5 w-5" />
+          </a>
+        </Hint>
+      </div>
     </aside>
   );
 };
