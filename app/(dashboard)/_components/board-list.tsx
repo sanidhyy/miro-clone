6b512c82199c764cc289882bbf7ef78bb1b8a550
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-08-29 11:23:50 +0530

    refactor: skeleton improvements

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -30,10 +30,10 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
           <NewBoardButton orgId={orgId} disabled />
-          <BoardCard.Skeleton />
-          <BoardCard.Skeleton />
-          <BoardCard.Skeleton />
-          <BoardCard.Skeleton />
+
+          {Array.from({ length: 4 }).map((_, i) => (
+            <BoardCard.Skeleton key={i} />
+          ))}
         </div>
       </div>
     );
