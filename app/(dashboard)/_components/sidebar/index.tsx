86cf04bf698c600e7c8074f4ec2e7f6b35da2711
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-08-30 08:03:07 +0530

    chore: replace github icon

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,29 +1,34 @@
 import { Github } from "lucide-react";
+import Link from "next/link";
 
 import { Hint } from "@/components/hint";
 import { links } from "@/config";
 
 import { List } from "./list";
-import { NewButton } from "./new-button";
+import Image from "next/image";
 
 export const Sidebar = () => {
   return (
     <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col justify-between text-white">
       <div className="flex flex-col gap-y-4">
         <List />
-        <NewButton />
       </div>
 
       <div className="aspect-square">
         <Hint label="Source Code" side="right" align="start" sideOffset={18}>
-          <a
+          <Link
             href={links.sourceCode}
             target="_blank"
             rel="noreferrer noopener"
-            className="bg-white/25 h-full w-full flex items-center justify-center rounded-md opacity-60 hover:opacity-100 transition"
+            className="size-full flex items-center justify-center rounded-md opacity-60 hover:opacity-100 transition"
           >
-            <Github className="text-white h-5 w-5" />
-          </a>
+            <Image
+              src="/github-white.png"
+              alt="GitHub"
+              width={24}
+              height={24}
+            />
+          </Link>
         </Hint>
       </div>
     </aside>
