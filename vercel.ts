a76ec552ee6c7b83af83f81e1919ad240fc86fde
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2026-02-23 13:53:10 +0530

    feat: setup vercel config

---
+++ vercel.ts
@@ -0,0 +1,7 @@
+import type { VercelConfig } from "@vercel/config/v1";
+
+export const config: VercelConfig = {
+  ignoreCommand:
+    "git diff HEAD^ HEAD --name-only | grep -qEv '(\.md$|LICENSE$|\.env\.example$|^\.github/|^\.vscode/)' && exit 1 || exit 0",
+  trailingSlash: false,
+};
